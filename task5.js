6db2063f9d1ed8a6fe20f2bbc0343bdd7eb75375
Jay8901909 <113981229+Jay8901909@users.noreply.github.com>
2023-06-29 15:19:56 +0530

    task5.js

---
+++ task5.js
@@ -0,0 +1,14 @@
+//Task 5: Arrays and Objects
+var favoriteBooks = [
+        "Its ends with us",
+        "Its start with us",
+        "Ugly",
+        "Wings of fire",
+    ];
+    function displayBookTitles(books) {
+     for (var i = 0; i &lt; books.length; i++) {
+        console.log(books[i]);
+         }
+     }
+    
+     displayBookTitles(favoriteBooks);
