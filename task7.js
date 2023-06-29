7ad07dda069bbc3eae0c0449d64e3c951f4b1460
Jay8901909 <113981229+Jay8901909@users.noreply.github.com>
2023-06-29 15:36:01 +0530

    task7.js

---
+++ task7.js
@@ -0,0 +1,20 @@
+//Task 7: DOM Manipulation
+var button = document.getElementById("myButton");
+button.addEventListener("click", function() {
+button.textContent = "Button Clicked!";
+});
+
+//Task 8: Error Handling
+function checkPositiveNumber(number) {
+ if (number < 0) {
+ throw new Error("Number must be positive.");
+ }
+
+ return number;
+}
+try {
+ var result = checkPositiveNumber(-5);
+ console.log("Result:", result);
+} catch (error) {
+console.log("Error:", error.message);
+}
